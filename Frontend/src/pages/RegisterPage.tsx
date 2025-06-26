4a144176cd4c5bb237c4391ecfdda5239edfa557
adam <adams.mos.5500@gmail.com>
2025-06-27 00:01:32 +0200

    Organize the code of Register Page

--- Frontend/src/pages/RegisterPage.tsx
+++ Frontend/src/pages/RegisterPage.tsx
@@ -3,47 +3,48 @@ import { useRef, useState } from "react";
 import { BASE_URL } from "../Constants/baseURL";
 
 const RegisterPage = () => {
-    const [error, setError] = useState("");
-    const firstNameRef = useRef<HTMLInputElement>(null);
-    const lastNameRef = useRef<HTMLInputElement>(null);
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
+  // use error
+  const [error, setError] = useState("");
 
-    const onSubmit = async() => {
-        const firstName = firstNameRef.current?.value;
-        const lastName = lastNameRef.current?.value;
-        const email = emailRef.current?.value;
-        const password = passwordRef.current?.value;
+  // The referances of INPUTS
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
-        console.log(firstName, lastName, email, password)
-        /// Make the call to API to create the user
-        const response = await fetch(`${BASE_URL}/user/register`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                firstName,
-                lastName,
-                email,
-                password
-            }),
-        });
-        if (!response.ok) {
+  // The function of sending data to the server
+  const onSubmit = async () => {
+    const firstName = firstNameRef.current?.value;
+    const lastName = lastNameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
 
-            setError("Email already exists, Please try again !")
-            return;
-        }
+    // test the inputs in log
+    console.log(firstName, lastName, email, password);
 
-        const data = await response.json();
-        console.log(data);
+    /// Make the call to API to create the user
+    const response = await fetch(`${BASE_URL}/user/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    if (!response.ok) {
+      setError("Email already exists, Please try again !");
+      return;
     }
 
+    const data = await response.json();
+    console.log(data);
+  };
 
-
-
-
-    ///////////////////////////////////////
+  /////////////////////// The Page
   return (
     <Container>
       <Box
@@ -67,12 +68,28 @@ const RegisterPage = () => {
             borderColor: "#f5f5",
           }}
         >
-          <TextField inputRef={firstNameRef} label="Full Name" name="fullName" />
+          <TextField
+            inputRef={firstNameRef}
+            label="Full Name"
+            name="fullName"
+          />
           <TextField inputRef={lastNameRef} label="Full Name" name="fullName" />
-          <TextField inputRef={emailRef} label="Email" type="email" name="email" />
-          <TextField inputRef={passwordRef} type="password" label="Password" name="password" />
-                  <Button onClick={onSubmit} variant="contained">Register</Button>
-                  {error && <Typography sx={{ color: "red" }}>{error }</Typography>}
+          <TextField
+            inputRef={emailRef}
+            label="Email"
+            type="email"
+            name="email"
+          />
+          <TextField
+            inputRef={passwordRef}
+            type="password"
+            label="Password"
+            name="password"
+          />
+          <Button onClick={onSubmit} variant="contained">
+            Register
+          </Button>
+          {error && <Typography sx={{ color: "red" }}>{error}</Typography>}
         </Box>
       </Box>
     </Container>
